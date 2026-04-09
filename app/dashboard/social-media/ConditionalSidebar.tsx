4b17b0ef6e1399589e6a8c2de147460a99aa5edd
AHAdd40451 <ahadkhan40451@gmail.com>
2026-04-09 11:24:03 -0400

    add socail media

---
+++ app/dashboard/social-media/ConditionalSidebar.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { AppSidebar } from "@/components/layout/sidebar/app-sidebar";
+
+export function ConditionalSidebar() {
+  const pathname = usePathname();
+
+  if (pathname === "/dashboard/social-media") {
+    return null;
+  }
+
+  return <AppSidebar variant="inset" />;
+}
